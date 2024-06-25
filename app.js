6bce499bfc242ded3c5b1a5a58571921398b8cc7
Enes Furkan SAGLAM <enesfurkansaglam@gmail.com>
2024-06-25 16:14:12 +0300

    The first part of the review is finished

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require('path');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-
 const yoneticiRoutes = require('./routes/yonetici');
 const doktorRoutes = require('./routes/doktor');
 const hastaRoutes = require('./routes/hasta');
@@ -15,20 +14,14 @@ const hastaRoutes = require('./routes/hasta');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.use('/yonetici',yoneticiRoutes);
 app.use('/doktor',doktorRoutes);
-app.use('/hasta',hastaRoutes);
+app.use('/patient',hastaRoutes);
 
 app.listen(5000,()=>{
-
     console.log('port 5000 aktif');
-
 });
-
-
-
-app.get('/giris', (req, res, next) => {
-    res.render('girisSayfasi.pug');
+app.get('/homepage', (req, res, next) => {
+    res.render('homePage.pug');
 });
 
--- routes/hasta.js
+++ routes/hasta.js
@@ -5,46 +5,41 @@ const Randevu = require('../models/randevu');
 
 var bizimHasta = 0;
 
-
-router.get('/secim', (req, res, next) => {
+router.get('/homepage', (req, res, next) => {
     res.render('hasta/secim.pug');
 });
-
 
 router.get('/hastaFormKaydet', (req, res) => {
     res.render('hasta/hastaFormKaydet', { hastalar: {} });
 });
-
 
 router.post('/kaydetHasta', async (req, res, next) => {
     const { ad, soyad, dogumTarihi, cinsiyet,telefon,adres } = req.body;
     try {
         const hasta = new Hasta(null,ad, soyad, dogumTarihi,cinsiyet, telefon,adres);
         await hasta.hastaKaydet();
-        res.redirect('/hasta/secim');
-    } catch (err) {
+        res.redirect('/patient/secim');
+    }catch (err) {
         console.error(err);
-        
-        res.redirect('/giris'); 
+        res.redirect('/homepage');
     }
 });
 
 
 
-
-router.get('/giris', (req, res, next) => {
+router.get('/login', (req, res, next) => {
     res.render('hasta/login'); 
 });
 
-router.post('/giris', async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
     const { username, password } = req.body;
     try {
         const result = await Hasta.hastaLogin(username, password);
         if (result.length > 0) {
             bizimHasta = password
-            res.redirect('/hasta/randevular'); 
+            res.redirect('/patient/appointments'); 
         } else {
-            res.redirect('/giris'); 
+            res.redirect('/homepage'); 
         }
     } catch (err) {
         console.error(err);
@@ -52,7 +47,7 @@ router.post('/giris', async (req, res, next) => {
 });
 
 
-router.get('/randevular', async (req, res, next) => {
+router.get('/appointments', async (req, res, next) => {
     try {
        const randevular = await Randevu.hastaRandevuListele(bizimHasta);
         res.render('hasta/randevuListele', {
@@ -64,11 +59,6 @@ router.get('/randevular', async (req, res, next) => {
         console.error(err);
     }
 });
-
-
-
-
-
 
 
 module.exports = router;
